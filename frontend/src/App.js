af317b429eef06b18397589c4c10bd3e70d3d181
PriyankVIT <priyankkaushik13@gmail.com>
2021-09-21 14:38:35 +0530

    Added Login and Signup Functionality with workign React App and Mongo DB Connection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Login from "./screens/Login";
+import Register from "./screens/Register";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Route path="/login" exact component={Login} />
+        <Route path="/register" exact component={Register} />
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
